5ff60e7c5ddcefcff5187b0a7e1f31ca60c24a0a
tyler <tcsuder@gmail.com>
2016-02-11 19:24:21 -0800

    add README and fix test.java file

---
+++ README.md
@@ -0,0 +1,28 @@
+# Word Puzzle
+
+#### A website that creates de-voweled words.
+
+#### By Tyler Suderman & Abby Rolling
+
+## Description
+
+This website accepts a user input and returns the input with vowels replaced with "-". The aim of the build was to show proficiency in BDD in Java as well as other fundamentals learned in week one of Epicodus Java course.
+
+## Known Bugs
+
+
+## Technologies Used
+
+* Java
+* HTML
+* Gradle
+* Spark
+* junit
+* Velocity
+* Fluentlenium
+
+### License
+
+Licensed under the GPL.
+
+Copyright (c) 2016 **Tyler Suderman**
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,35 +1,44 @@
 import org.junit.*;
 import static org.junit.Assert.*;
-
 import java.util.ArrayList;
-
 import org.fluentlenium.adapter.FluentTest;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class AppTest {
+public class ChangeTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+  public WebDriver getDefaultDriver() {
+      return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
 // Unit testing
+  // @Test
+  // public void changeCalculator_convertsTwentyfivePenniesTo_OneQuarter() {
+  //   Change testChange = new Change();
+  //   Integer[] change = {1, 0, 0, 0};
+  //   Integer[] tillContents = {100, 100, 100, 100};
+  //   assertEquals(change, testChange.changeCalculator(25, tillContents));
+  // }
 
 
-
-//Integration testing
-  public class AppTest extends FluentTest {
-    public WebDriver webDriver = new HtmlUnitDriver();
-    public WebDriver getDefaultDriver() {
-        return webDriver;
+  //Integration testing
+    @Test
+    public void rootTest() {
+        goTo("http://localhost:4567/change");
+        assertThat(pageSource()).contains("Input the number of cents");
     }
 
-    @ClassRule
-    public static ServerRule server = new ServerRule();
-
     // @Test
-    // public void rootTest() {
-    //     goTo("http://localhost:4567/");
-    //     assertThat(pageSource()).contains("Leap year detector");
+    // public void fillFormTest() {
+    //     goTo("http://localhost:4567/change");
+    //     fill("#changeInput").with("33");
+    //     submit("#submit");
+    //     assertThat(pageSource()).contains("Your change for");
     // }
-  }
 }
